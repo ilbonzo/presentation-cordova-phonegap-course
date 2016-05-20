15104aed0cc042dcb6a4aeee6ae95b9571624bc8
Matteo Magni <matteo@magni.me>
2016-05-20 14:53:44 +0200

    Generated by generator-bespoke

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,35 @@ bespoke.from('article', {
   state: true,
   forms: true
 });
+
+
+(function(){
+
+  bespoke.plugins.backgroundElement = function(deck) {
+    var el = document.createElement('div');
+    el.className = 'background';
+    deck.parent.appendChild(el);
+  };
+
+  (function preloadBackgroundImages() {
+
+    var matches, image,
+      forEach = function(arrayLike, fn) {
+        [].slice.call(arrayLike, 0).forEach(fn);
+      };
+
+    forEach(document.styleSheets, function(sheet) {
+      forEach(sheet.rules, function(rule) {
+        if (rule.style && rule.style.backgroundImage) {
+          matches = rule.style.backgroundImage.match(/url\(\"(.*)\"\)/);
+          if (matches) {
+            image = new Image();
+            image.src = matches[1];
+          }
+        }
+      });
+    });
+
+  }());
+
+}());
